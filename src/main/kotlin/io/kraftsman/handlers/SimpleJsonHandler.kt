de957e02c09662eaa674b9d5be2d90d270797ccf
Shengyou Fan <shengyoufan@gmail.com>
2021-04-07 10:56:55 +0800

    restrict http method

---
+++ src/main/kotlin/io/kraftsman/handlers/SimpleJsonHandler.kt
@@ -0,0 +1,24 @@
+package io.kraftsman.handlers
+
+import com.google.cloud.functions.HttpFunction
+import com.google.cloud.functions.HttpRequest
+import com.google.cloud.functions.HttpResponse
+import java.io.IOException
+import java.net.HttpURLConnection
+import kotlin.jvm.Throws
+
+class SimpleJsonHandler: HttpFunction {
+
+    @Throws(IOException::class)
+    override fun service(request: HttpRequest, response: HttpResponse) {
+
+        if ("GET" != request.method.toUpperCase()) {
+            with(response) {
+                setStatusCode(HttpURLConnection.HTTP_BAD_METHOD)
+                writer.write("Bad method")
+            }
+
+            return
+        }
+    }
+}
