625283f0637e5b01c6c0fbcff4ec8964c5b8eb6c
Shengyou Fan <shengyoufan@gmail.com>
2021-05-15 18:49:19 +0800

    add deploy scripts

---
+++ windows-deploy.ps1
@@ -0,0 +1,8 @@
+gcloud functions deploy text `
+  --region asia-east2 `
+  --runtime java11 `
+  --trigger-http `
+  --memory 256MB `
+  --source build\deploy `
+  --entry-point io.kraftsman.handlers.PlainTextHandler `
+  --allow-unauthenticated
